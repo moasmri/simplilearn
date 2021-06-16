d95f72a93462624c5a0a3dee27c87bdeb49c86e6
abusamrah2005 <abusamrah2005@hotmail.com>
2021-06-17 00:49:03 +0300

    Define file path & Show Option To user To select one of Them 1-2-3-4

--- Teacher_Records_System/Program.cs
+++ Teacher_Records_System/Program.cs
@@ -10,13 +10,20 @@ namespace Teacher_Records_System
             Console.ReadKey();
         }
 
-        static void welcome()
+        static void welcome() // first show welcome message
         {
             Console.ForegroundColor = ConsoleColor.Green;
           
             Console.WriteLine(@"Welcome to my little project It is an example of a system for storing teachers' data Sponsored by Simpliearn");
             
             Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        static void Teacher_Records()
+        {
+
+            bool system_status = true;
+            string txtPath = @"C:\Users\moasmri\Desktop\ConsoleAppTeacher\ConsoleAppTeacher\moasmrit.txt";// txt file path and name
         }
     }
 }
